c3c389a8dd29028f88eed6924d1edea1d452ec67
Hopson Gausi <hopgausi20@gmail.com>
2022-08-29 12:57:03 +0200

    updated mapping

--- bs430.js
+++ bs430.js
@@ -16,9 +16,9 @@ const BASE_URL = settings.iblsBaseURL;
 console.log(`========= SERVICE STARTED ON PORT ${PORT} =========`);
 hl7Service.use(function(req, res, next) {
     console.log('===CONNECTED SUCCESSFULY TO BS430 MINDRAY MACHINE====');
-    console.log(req.msg.log());
-    let sampleID = req.msg.getSegment('OBR').fields[2].value[0];
-    // var sampleID = req.msg.getSegment('PID').fields[1].value[0][0].value[0];
+    console.log(`${req.msg.log()} \n`);
+    // let sampleID = req.msg.getSegment('OBR').fields[2].value[0];
+    let sampleID = req.msg.getSegment('PID').fields[1].value[0][0].value[0];
     console.log(`Sample ID: ${sampleID}`);
     let results = req.msg.getSegments('OBX');
     results.forEach(result => {
